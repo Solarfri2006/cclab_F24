5e05c7128a89bd2b3bd0765de55723c4b32eb8b4
Solarfri2006 <yy5439@nyu.edu>
2024-10-23 12:17:35 +0800

    updated project description & code

--- project_A/script.js
+++ project_A/script.js
@@ -1,61 +1,56 @@
-let s =30
+let s = 30;
 let groupX;
 let groupY;
 let wingAngle = 0;
-let oscillationAngle=0
+let oscillationAngle = 0;
+
+let debrisX;
+let debrisY;
+let showDebris = false;
+
 function setup() {
   let canvas = createCanvas(800, 500);
-  canvas.parent("p5_canvas_container");
-  colorMode(HSB)
-  groupX=width/2
-  groupY=height/2
-  
-  
-  
+  canvas.id("p5-canvas")
+  canvas.parent("p5-canvas-container")
+  colorMode(HSB);
+  groupX = width / 2;
+  groupY = height / 2;
 }
 
 function draw() {
-  
-  for (let i = 0; i < width; i += s) {
-    for (let j = 0; j < height; j += s) {
-      stroke(random(255), random(255), random(255));
-      noStroke();
-      fill(random(200,240), random(40,60), 100);
-      rect(i, j, s, s);
-    }
-  }
+  background(260, 100, 70);
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 10; i++) {
     let x1 = random(width / 5);
     let y1 = height - random(20, 200);
     let r = random(2, 60);
-    stroke(random(0,360),random(0,100),random(0,100));
+    stroke(random(0, 360), random(0, 100), random(0, 100));
     strokeWeight(random(3, 10));
-    fill(random(340,360), 65, random(97, 140));
+    fill(random(340, 360), 65, random(97, 140));
     square(x1, y1, r);
   }
 
-  for (let i = 0; i < 70; i++) {
+  for (let i = 0; i < 20; i++) {
     let x1 = random((3 * width) / 5, width);
     let y1 = height - random(40, 400);
     let r = random(10, 70);
     stroke(random(255), random(255), random(255));
     strokeWeight(random(3, 10));
-    fill(random(340,360), 65, random(97, 140));
+    fill(random(340, 360), 65, random(97, 140));
     square(x1, y1, r);
   }
 
-  for (let i = 0; i < 50; i++) {
+  for (let i = 0; i < 30; i++) {
     let x3 = random(200, 300);
     let y3 = random(height);
     r1 = random(1, 100);
     noFill();
-    stroke(random(85,140),random(50,100),55);
+    stroke(random(85, 140), random(50, 100), 55);
     strokeWeight(random(0, 5));
     circle(x3, y3, r1);
   }
 
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 10; i++) {
     let x4 = random(450, 500);
     let y4 = random(height);
     r2 = random(1, 50);
@@ -64,40 +59,56 @@ function draw() {
     strokeWeight(1, 10);
     circle(x4, y4, r2);
   }
-  
-  if (mouseIsPressed) {
-    groupX = lerp(groupX, mouseX, 0.05);
-    groupY = lerp(groupY, mouseY, 0.05);
-  } else {
-    
-    
-    
-    // Apply oscillation for gliding back and forth motion
-    let oscillationX = 30 * sin(oscillationAngle)
-    let oscillationY = 20 * cos(oscillationAngle)
-    oscillationAngle += 0.05;
-    groupX += (noise(0.05*frameCount))*oscillationX*0.5;
-    groupY += (noise(0.05*frameCount))*oscillationY*0.5;
-    
-    groupX = constrain(groupX, 0, width)
-    groupY = constrain(groupY, 0, height)
+
+  if (keyIsPressed) {
+    generateDebris();
+    showDebris = true;
   }
-  
-  wingAngle=PI/6*sin(frameCount*0.1)
 
-  
-  drawNeon(groupX, groupY)
+  if (showDebris) {
+    drawDebris();
+  }
 
-  
+  if (showDebris) {
+    let dx = debrisX - groupX;
+    let dy = debrisY - groupY;
+    let distDN = dist(debrisX, debrisY, groupX, groupY);
+
+    if (distDN > 10) {
+      groupX += (dx / distDN) * 7;
+      groupY += (dy / distDN) * 7;
+    } else {
+      showDebris = false;
+    }
+  } else {
+    if (mouseIsPressed) {
+      groupX = lerp(groupX, mouseX, 0.05);
+      groupY = lerp(groupY, mouseY, 0.05);
+    } else {
+      // Apply oscillation for gliding back and forth motion
+      let oscillationX = 30 * sin(oscillationAngle);
+      let oscillationY = 20 * cos(oscillationAngle);
+      oscillationAngle += 0.05;
+      groupX += noise(0.05 * frameCount) * oscillationX * 0.5;
+      groupY += noise(0.05 * frameCount) * oscillationY * 0.5;
+
+      groupX = constrain(groupX, 0, width + 100);
+      groupY = constrain(groupY, 0, height + 100);
+    }
+  }
+
+  wingAngle = (PI / 6) * sin(frameCount * 0.1);
+
+  drawNeon(groupX, groupY);
 }
 
-function drawNeon(x,y){
+function drawNeon(x, y) {
   push();
   translate(x, y);
 
   // Body
   noStroke();
-  fill(random(265, 335), 50, 85);
+  fill(random(55, 180), 50, 100);
   circle(0, 0, 120);
 
   // Eyes
@@ -107,13 +118,13 @@ function drawNeon(x,y){
 
   noStroke();
   fill(46, 97, 91);
-  circle(-15, -5, 30);  // Left eye
-  circle(15, -5, 30);   // Right eye
+  circle(-15, -5, 30); // Left eye
+  circle(15, -5, 30); // Right eye
 
   // Pupils
   fill(0);
-  circle(-15, -5, 10);  // Left pupil
-  circle(15, -5, 10);   // Right pupil
+  circle(-15, -5, 10); // Left pupil
+  circle(15, -5, 10); // Right pupil
 
   // Beak
   fill(45, 100, 20);
@@ -130,7 +141,7 @@ function drawFlappingWings() {
   push();
   translate(-10, 0);
   rotate(wingAngle);
-  fill(random(265, 335), 50, 85);
+  fill(random(55, 180), 70, 100);
   triangle(-150, 70, -90, -20, -50, 0);
   pop();
 
@@ -138,7 +149,25 @@ function drawFlappingWings() {
   push();
   translate(10, 0);
   rotate(-wingAngle);
-  fill(random(265, 335), 50, 85);
+  fill(random(55, 180), 70, 100);
   triangle(150, 70, 90, -20, 50, 0);
   pop();
+}
+
+function generateDebris() {
+  debrisX = random(0, width);
+  debrisY = random(0, height);
+}
+
+function drawDebris() {
+  stroke(random(0, 10));
+  fill(random(0, 360));
+  triangle(
+    debrisX,
+    debrisY,
+    debrisX + random(-40, 40),
+    debrisY + random(-40, 40),
+    debrisX + random(-40, 40),
+    debrisY + random(-40, 40)
+  );
 }
